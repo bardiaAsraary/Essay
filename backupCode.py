ffa0c996901d97b64fd5a0d095b04d4c634a972c
Bardia <your.email@university.edu>
2025-05-09 11:36:48 +0200

    final touch to mode1

--- backupCode.py
+++
@@ -1,8 +0,0 @@
-# Recursively list files and directories under /content
-!find /content -type f
-# Zip the entire /content folder (or a subfolder)
-!zip -r content_backup.zip /content
-
-# Download the ZIP
-from google.colab import files
-files.download('content_backup.zip')
